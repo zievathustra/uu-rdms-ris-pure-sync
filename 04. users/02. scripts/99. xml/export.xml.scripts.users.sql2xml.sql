e3adfb1ca2ce0c52ad9660000e17df44d7c5072c
Arjan Sieverink <j.a.sieverink@uu.nl>
2020-06-21 11:35:47 +0200

    Exclude persons without username and/or email.

--- 04. users/02. scripts/99. xml/export.xml.scripts.users.sql2xml.sql
+++ 04. users/02. scripts/99. xml/export.xml.scripts.users.sql2xml.sql
@@ -9,15 +9,14 @@ WITH xmlnamespaces(
       'v1.user-sync.pure.atira.dk' as v1,
       'v3.commons.pure.atira.dk' as v3)
 SELECT @x = (
-SELECT 
-      tblData.person_id as "@id",
-      tblData.username as "v1:userName",
-      tblData.email as "v1:email",
-      tblData.first_name as "v1:name/v3:firstname",
-      tblData.last_name as "v1:name/v3:lastname"
-FROM [dbo].[PERSON_SAPDATA] as tblData
-WHERE ISNULL(tblData.username, '') <> ''
-ORDER BY tblData.person_id
+SELECT
+      vwPersons.person_id as "@id",
+      vwPersons.username as "v1:userName",
+      vwPersons.email as "v1:email",
+      vwPersons.first_name as "v1:name/v3:firstname",
+      vwPersons.last_name as "v1:name/v3:lastname"
+FROM [dbo].[PERSON_DATA] as vwPersons
+ORDER BY vwPersons.person_id
 FOR XML PATH('v1:user'), ROOT('v1:users'))
 
 EXEC dbo.XMLExportToFile @x, @file
